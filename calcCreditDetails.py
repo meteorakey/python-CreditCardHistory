21b281a18df155ca22113019fa6a3aa0558e78d4
meteorakey <meteorakey@gmail.com>
2018-02-06 20:20:12 +0900

    feat:sort result by key value

--- calcCreditDetails.py
+++ calcCreditDetails.py
@@ -2,17 +2,15 @@
 #coding: utf-8
 import sys
 
-argvs = sys.argv
-flg = False
-dict = {}
-
 # check argv length
+argvs = sys.argv
 if(len(argvs) != 2):
     print "usage: python calcCreditDetails.py target.csv"
     print "4th col:dest, 5th col:amount"
     sys.exit()
 
 # loop each csv line
+dict = {}
 for line in open(argvs[1], 'r'):
     strs = line[:-1].split(',')
     if (dict.has_key(strs[4])):
@@ -22,8 +20,8 @@ for line in open(argvs[1], 'r'):
 
 # output result data
 f = open("./result.csv", "w")
-for key in dict:
-    result = key + ",  " + str(dict[key])
+for k,v in sorted(dict.items()):
+    result = str(k) + ",  " + str(v)
     print result
     f.write(result)
 f.close()
