389f3a286e1315e440b2dc0c984b9a4916c1f32d
meteorakey <meteorakey@gmail.com>
2018-02-06 19:59:05 +0900

    cleanup

--- LoadCSV.py
+++ LoadCSV.py
@@ -1,30 +1,29 @@
 #!/usr/bin python
 #coding: utf-8
-# LifeCardの明細CSVを読み込んで合算
 import sys
 
 argvs = sys.argv
 flg = False
 dict = {}
 
-if(len(argvs) < 2):
-    print "select csv file"
+# check argv length
+if(len(argvs) != 2):
+    print "usage: python calcCreditDetails.py target.csv"
+    print "4th col:dest, 5th col:amount"
     sys.exit()
 
+# loop each csv line
 for line in open(argvs[1], 'r'):
     strs = line[:-1].split(',')
-    if (len(strs) < 2):
-        flg = False
-    if (flg == True):
-        if (dict.has_key(strs[4])):
-            dict[strs[4]] = dict[strs[4]] + int(strs[5])
-        else:
-            dict[strs[4]] = int(strs[5])
-    if (strs[0] == "明細No."):
-        flg = True
+    if (dict.has_key(strs[4])):
+        dict[strs[4]] = dict[strs[4]] + int(strs[5])
+    else:
+        dict[strs[4]] = int(strs[5])
 
-f = open("CardHistorySum.csv", 'a')
+# output result data
+f = open("./result.csv", "w")
 for key in dict:
-    print key + ", " + str(dict[key])
-    f.write(key + ", " + str(dict[key]) + "\n")
+    result = key + ",  " + str(dict[key])
+    print result
+    f.write(result)
 f.close()
