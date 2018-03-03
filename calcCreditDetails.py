1d1c5b94c953c71a41b7381c45d81d3c578bf3b0
meteorakey <meteorakey@gmail.com>
2018-03-03 13:45:14 +0900

    feat:convert csv to utf-8 & LF

--- calcCreditDetails.py
+++ calcCreditDetails.py
@@ -1,6 +1,7 @@
 #!/usr/bin python
 #coding: utf-8
 import sys
+import subprocess
 
 # check argv length
 argvs = sys.argv
@@ -9,9 +10,14 @@ if(len(argvs) != 2):
     print "4th col:dest, 5th col:amount"
     sys.exit()
 
+# convert csv utf-8, LF
+csv = argvs[1]
+_nkf = 'nkf -wd --overwrite ' + csv
+subprocess.check_call(_nkf, shell=True) 
+
 # loop each csv line
 dict = {}
-for line in open(argvs[1], 'r'):
+for line in open(csv, 'r'):
     strs = line[:-1].split(',')
     if (dict.has_key(strs[4])):
         dict[strs[4]] = dict[strs[4]] + int(strs[5])
@@ -23,5 +29,5 @@ f = open("./result.csv", "w")
 for k,v in sorted(dict.items()):
     result = str(k) + ",  " + str(v)
     print result
-    f.write(result)
+    f.write(result + "\n")
 f.close()
